53301b36897a88fddb2c15a885f339bb92ee73bc
Dante Siegel <dantesiegel@gmail.com>
2024-06-17 00:03:01 -0300

    negra

--- proyecto-final-next/app/components/CardNovedades/index.js
+++ proyecto-final-next/app/components/CardNovedades/index.js
@@ -1,22 +1,33 @@
-// components/CardRecipe.js
+// components/CardNovedades/index.js
 
 import React from 'react';
 import styles from './styles.module.css';
 import Image from 'next/image';
 import BookmarkIcon from '../../img/bookmark.png';
+import PerfilImagen from '../../img/neo.jpg'
 
-const CardRecipe = ({ name, image, time, calories, price, user }) => {
+const CardRecipe = ({ name, image, time, calories, price, user, userImage }) => {
   return (
     <div className={styles.card}>
+      <Image src={image} alt={name} className={styles.foodImage} width={150} height={150} />
       <h3 className={styles.name}>{name}</h3>
-      <Image src={image} alt={name} width={200} height={150} />
       <div className={styles.details}>
-        <p className={styles.info}>Tiempo: {time}</p>
-        <p className={styles.info}>Calorías: {calories}</p>
-        <p className={styles.info}>Precio: {price}</p>
+        <div className={styles.info}>
+          <p className={styles.tiempo}>Tiempo</p>
+          <p className={styles.mins}>{time}</p>
+        </div>
+        <div className={styles.info}>
+          <p className={styles.tiempo}>Calorías</p>
+          <p className={styles.mins}>{calories}</p>
+        </div>
+        <div className={styles.info}>
+          <p className={styles.tiempo}>Precio</p>
+          <p className={styles.mins}>{price}</p>
+        </div>
       </div>
       <div className={styles.user}>{user}</div>
-      <Image src={BookmarkIcon} alt="Bookmark" className={styles.bookmark} />
+      <Image src={PerfilImagen} alt="ImageUser" className={styles.userImage} width={20} height={20} />
+      <Image src={BookmarkIcon} alt="Bookmark" className={styles.bookmark} width={20} height={20} />
     </div>
   );
 };
